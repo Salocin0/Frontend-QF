678d4a7a0c1a8afb3ffa09f61d43a28989d0f0f5
nachomsjz <nachosachetto1998@hotmail.com>
2023-08-14 11:51:19 -0300

    Recuperar Contraseña proto 50%

--- src/components/ComponentesLogin/RecuperarContraseña.js
+++ src/components/ComponentesLogin/RecuperarContraseña.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import Footer from '../ComponentesGenerales/Footer';
+import './RecuperarContraseña.css';
 
 const RecuperarContraseña = () => {
   const [email, setEmail] = useState('');
@@ -14,62 +16,77 @@ const RecuperarContraseña = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const json_recuperarEmail ={
-      correoElectronico:email
-    }
+    const json_recuperarEmail = {
+      correoElectronico: email,
+    };
 
     fetch('http://127.0.0.1:8000/user/recuperarcontrasenia', {
       method: 'PUT',
-      headers: {'Content-Type': 'application/json',},
-      body: JSON.stringify(json_recuperarEmail)
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(json_recuperarEmail),
     })
-      .then(response => response.json()).then(data => {
-        if (data.code===200){
-          toast.success("Email enviado");
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.code === 200) {
+          toast.success('Email sent');
           navigate(`/`);
-        }else if(data.code===400){
-          toast.error("Error al recuperar contraseña");
+        } else if (data.code === 400) {
+          toast.error('Error recovering password');
         }
-    })
-      .catch(error => {
+      })
+      .catch((error) => {
         console.error(error);
-    });
+      });
   };
 
   return (
     <>
-    <section className="vh-100 d-flex align-items-center justify-content-center" style={{ background: 'url(QuickFoodFondo.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <div className="container">
-        <div className="row justify-content-sm-center">
-          <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-            <div className="card shadow-lg">
-              <div className="card-body p-5">
-                <h1 className="fs-4 card-title fw-bold mb-4 text-black">Recuperar Contraseña</h1>
-                <form onSubmit={handleSubmit} className="needs-validation">
-                  <div className="mb-3">
-                    <label className="mb-2 text-muted" htmlFor="email">
-                      Ingrese su correo electrónico:
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      className="form-control"
-                      value={email}
-                      onChange={handleEmailChange}
-                      required
-                    />
-                  </div>
-                  <div className="d-grid">
-                    <button type="submit" className="btn btn-primary">Enviar correo de recuperación</button>
-                  </div>
-                </form>
+      <section className="vh-100 d-flex align-items-center justify-content-center bg-black">
+        <div className="container ">
+          <div className="row justify-content-sm-center">
+            <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9 border-0">
+              <div className="card">
+                <div className="card-top bg-secondary rounded-top">
+                  <h1 className="fs-4 card-title fw-bold text-black text-center mt-3 mb-4 rounded-pill">Recuperar Contraseña</h1>
+                </div>
               </div>
             </div>
           </div>
+          <div className="row justify-content-center mt-3">
+            <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+              <div className="card shadow-lg">
+                <div className="card-body p-4 bg-secondary rounded-lg">
+                  <form onSubmit={handleSubmit} className="needs-validation">
+                    <div className="mb-3">
+                      <div className="input-group">
+                        <span className="input-group-text ">
+                        <FontAwesomeIcon icon="fa-solid fa-lock" style={{color: "#cbb315",}} />
+                        </span>
+                        <input
+                          type="email"
+                          id="email"
+                          className="form-control rounded-pill input-gris"
+                          value={email}
+                          onChange={handleEmailChange}
+                          placeholder="Ingresa tu Correo"
+                          required
+                        />
+                      </div>
+
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center mt-3">
+            <button type="submit" className="btn btn-warning botonRecuperacion">
+              Enviar Correo de Recuperación
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
-    <Footer/>
+      </section>
+      <Footer />
     </>
   );
 };
